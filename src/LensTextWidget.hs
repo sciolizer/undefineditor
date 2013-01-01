99a495871c64fc374312c4b8e7ae53ee2892d7eb
Joshua Ball <JoshBBall@gmail.com>
2012-12-31 22:58:28 -0800

    fsalkj

--- src/LensTextWidget.hs
+++ src/LensTextWidget.hs
@@ -1,12 +1,15 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE TemplateHaskell #-}
 module LensTextWidget where
 
 import Control.Lens
+import Control.Monad
 import qualified Data.Sequence as S
 
 data FileCoord = FileCoord {
   _fileRow :: Int,
   _fileColumn :: Int }
+  deriving (Eq, Ord, Read, Show)
 
 data PortalCoord = PortalCoord {
   _portalRow :: Int,
@@ -26,6 +29,7 @@ type UpperLeft = FileCoord -- where the view starts
 type Cursor
 -}
 
+-- should always be non-empty
 data Buffer = Buffer { _seqs :: S.Seq (S.Seq Char) }
   deriving (Eq, Ord, Read, Show)
 
@@ -39,9 +43,28 @@ render :: Columns -> UpperLeft -> Getter Buffer [(Bool, String)]
 
 -}
 
+-- concatenated :: Simple Iso (S.Seq (S.Seq a)) (S.Seq a)
+concatenated = iso combine split where
+  combine ss =
+    case S.viewr . join . fmap (S.|> '\n') $ ss of
+      S.EmptyR -> internalBug "outer sequence of buffer should never be empty"
+      ret S.:> _ -> ret
+  split = S.unfoldr chunk
+  chunk rest =
+    case S.spanl (/= '\n') rest of
+      (left, right) ->
+        case S.viewl right of
+          ('\n' S.:< r) -> Just (left, r)
+          (_ S.:< _) -> internalBug "impossible: front should have been \\n"
+          S.EmptyL -> Nothing
+
+internalBug = error
+
 -- use this for inserts and deletes
-slicedFromCoord :: FileCoord -> Simple Lens Buffer (S.Seq (S.Seq Char))
-slicedFromCoord (FileCoord r c) = seqs.(lens get set) where
+{-
+slicedBetween :: FileCoord -> FileCoord -> Simple Lens Buffer (S.Seq Char)
+slicedBetween fca fcb = seqs.(lens get set) where
+  (fc1, fc2) = if fca < fcb then (fca, fcb) else (fcb, fca)
   parts ss =
     case S.splitAt r ss of
       (bfLine, right) ->
@@ -55,3 +78,4 @@ slicedFromCoord (FileCoord r c) = seqs.(lens get set) where
       S.EmptyL -> error $ "replacement is empty; if you really want to delete to the end, use Data.Sequence.singleton Data.Sequence.empty"
       (x S.:< xs) -> bfLine S.>< ((bfChar S.>< x) S.<| xs)
     where (bfLine, (bfChar, _), _) = parts ss
+    -}
