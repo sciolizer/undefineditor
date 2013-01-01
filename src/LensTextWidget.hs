d7eff1850715c1311bdcd31003cbcf8e8a226eba
Joshua Ball <JoshBBall@gmail.com>
2013-01-01 07:48:43 -0800

    ok, finally sastisfied the laws

--- src/LensTextWidget.hs
+++ src/LensTextWidget.hs
@@ -9,12 +9,12 @@ import qualified Data.Sequence as S
 
 data FileCoord = FileCoord {
   _fileRow :: Int,
-  _fileColumn :: Int }
+  _fileCol :: Int }
   deriving (Eq, Ord, Read, Show)
 
 data PortalCoord = PortalCoord {
   _portalRow :: Int,
-  _portalColumn :: Int -- hidden column; visible cursor might be further left if the line is too short
+  _portalCol :: Int -- hidden column; visible cursor might be further left if the line is too short
   }
 
 {-
@@ -31,10 +31,7 @@ type Cursor
 -}
 
 -- should always be non-empty
-data Buffer = Buffer { _seqs :: S.Seq (S.Seq Char) }
-  deriving (Eq, Ord, Read, Show)
-
-makeLenses ''Buffer
+type Buffer = SS Char
 
 {-
 portal :: Buffer -> Columns -> Simple Iso FileCoord CursorCoord
@@ -69,10 +66,11 @@ internalBug = error
 type S = S.Seq
 type SS a = S.Seq (S.Seq a)
 
+type Slice = (Buffer, FileCoord {- end -})
+
 -- use this for inserts and deletes
-slicedBetween :: FileCoord -> FileCoord -> Simple Lens Buffer (S Char)
-slicedBetween fca fcb = seqs.(lens get set).concatenated where
-  (fc1, fc2) = if fca < fcb then (fca, fcb) else (fcb, fca)
+slice :: FileCoord {- start -} -> Simple Lens Slice (S Char)
+slice fc1 = lens get set . concatenated where
   parts :: SS a -> FileCoord -> (SS a, (S a, S a), SS a)
   parts ss (FileCoord r c) =
     case S.splitAt r ss of
@@ -80,19 +78,19 @@ slicedBetween fca fcb = seqs.(lens get set).concatenated where
         case S.viewl right of
           S.EmptyL -> error $ "coordinate is past last line: " ++ show r ++ ", " ++ show c
           (x :< xs) -> (bfLine, S.splitAt c x, xs)
-  splitRange :: SS a -> (SS a, (S a, S a), SS a, (S a, S a), SS a)
-  splitRange ss = (left, (leftFst, leftSnd), middle, (rightFst, rightSnd), right) where
+  splitRange :: FileCoord {- end -} -> SS a -> (SS a, (S a, S a), SS a, (S a, S a), SS a)
+  splitRange fc2 ss = (left, (leftFst, leftSnd), middle, (rightFst, rightSnd), right) where
     (front, (rightFst, rightSnd), right) = parts ss fc2
     (left, (leftFst, leftSnd), middle) = parts front fc1
-  get ss = (leftSnd <| middle) |> rightFst where
-    (_, (_, leftSnd), middle, (rightFst, _), _) = splitRange ss
-  set ss repl =
+  get (ss, fc2) = (leftSnd <| middle) |> rightFst where
+    (_, (_, leftSnd), middle, (rightFst, _), _) = splitRange fc2 ss
+  set (ss, fc2) repl =
     case S.viewl repl of
       S.EmptyL -> internalBug $ "replacement is empty; should be at least a singleton of empty; probably a bug in `concatenated`"
       (x :< xs) -> let
-        middle =
+        (middle, fc2') =
           case S.viewr xs of
-            S.EmptyR -> S.singleton $ leftFst >< x >< rightSnd
-            (ys :> y) -> ((leftFst >< x) <| ys) |> (y >< rightSnd) in
-        left >< middle >< right where
-          (left, (leftFst, _), _, (_, rightSnd), right) = splitRange ss
+            S.EmptyR -> (S.singleton $ leftFst >< x >< rightSnd, FileCoord (_fileRow fc1) (S.length x + _fileCol fc1))
+            (ys :> y) -> (((leftFst >< x) <| ys) |> (y >< rightSnd), FileCoord (_fileRow fc1 + S.length ys + 1) (S.length y)) in
+        (left >< middle >< right, fc2') where
+          (left, (leftFst, _), _, (_, rightSnd), right) = splitRange fc2 ss
