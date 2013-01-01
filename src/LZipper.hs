4c316a656976015067bfc97cd33b7885e19146f1
Joshua Ball <JoshBBall@gmail.com>
2012-12-31 20:18:17 -0800

    Finally I found the right data structure

--- src/LZipper.hs
+++ src/LZipper.hs
@@ -9,23 +9,21 @@ module LZipper where
 import Control.Applicative
 -- import qualified Control.Lens as L
 import Control.Lens hiding (Tape, rezip, saveTape, unsafelyRestoreTape, zipper)
+import Data.Sequence.Lens
 import Control.Monad
--- import Data.List.Split
-
-chunkDefault = 10
-chunkLimit = 20
-
-data Tree a = Node a | Pair (Tree a) (Tree a)
+import Data.Foldable
+import qualified Data.Sequence as S
+import Data.Sequence ((><), ViewL((:<)))
 
 data LZipper a = LZipper {
-  _before :: (Int, [a]), -- int is just size
-  _after :: (Int, [a]) -- head of after is current element
+  _before :: S.Seq a,
+  _after :: S.Seq a
   }
 
 makeLenses ''LZipper
 
 zipper :: [a] -> LZipper a
-zipper xs = LZipper (0, []) (length xs, xs)
+zipper xs = LZipper S.empty (S.fromList xs)
 
 -- instance Indexable (Tape a) k
 -- if the value is a Just, you can only set a Just
@@ -34,27 +32,27 @@ zipper xs = LZipper (0, []) (length xs, xs)
 focus :: SimpleIndexedLens (Tape a) (LZipper a) (Maybe a)
 focus = indexed foo where -- $ \f lz@(LZipper (i,_) _) -> swapIn lz <$> f (Tape i) (current lz)
   foo :: (Functor f) => (Tape a -> (Maybe a) -> f (Maybe a)) -> LZipper a -> f (LZipper a)
-  foo f lz@(LZipper (i,_) _) = swapIn lz <$> f (Tape i) (current lz)
+  foo f lz@(LZipper bf _) = swapIn lz <$> f (Tape (S.length bf)) (current lz)
   swapIn :: LZipper a -> Maybe a -> LZipper a
-  swapIn lz val = over (after._2) changeHead lz where
-    changeHead [] =
+  swapIn lz val = over (after.viewL) changeHead lz where
+    changeHead S.EmptyL =
       case val of
-        Nothing -> []
+        Nothing -> S.EmptyL
         Just _ -> invalidArgument "cannot set at end of list; use insert instead"
-    changeHead (_:xs) =
+    changeHead (_ :< xs) =
       case val of
         Nothing -> invalidArgument "cannot set Nothing in the middle of the list; use delete instead"
-        Just x' -> (x':xs)
+        Just x' -> x' :< xs
   current :: LZipper a -> Maybe a
-  current = view (after . _2 . to safeHead) where
-    safeHead [] = Nothing
-    safeHead (x:_) = Just x
+  current = view (after . viewL . to safeHead) where
+    safeHead S.EmptyL = Nothing
+    safeHead (x :< _) = Just x
 
 invalidArgument = error
 
 -- usage: over myZipper (insert 'x')
 insert :: a -> LZipper a -> LZipper a
-insert x = over after (\(!i,xs) -> (i + 1, x : xs))
+insert x = undefined -- over after (\(!i,xs) -> (i + 1, x : xs))
 
 -- deleting at the end is a no-op
 delete :: LZipper a -> LZipper a
@@ -85,7 +83,7 @@ tugTo :: Int -> LZipper a -> LZipper a
 tugTo = undefined
 
 rezip :: LZipper a -> [a]
-rezip (LZipper (_,b) (_,a)) = reverse b ++ a
+rezip (LZipper l r) = toList (l >< r)
 
 data Tape a = Tape Int
 
